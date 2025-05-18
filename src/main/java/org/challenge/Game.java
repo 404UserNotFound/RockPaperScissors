d372112b545083e00bdd11da0215ad9079a7dbf5
Mih <mihaelabrodetchi@gmail.com>
2025-05-18 16:05:40 +0100

    *game logic: loop through totalNumber of rounds, grab playerMove, generate computerMove, and evaluate result for each round

--- src/main/java/org/challenge/Game.java
+++ src/main/java/org/challenge/Game.java
@@ -3,6 +3,8 @@ package org.challenge;
 import org.challenge.computer.ComputerMoveGenerator;
 import org.challenge.evulator.MoveEvaluator;
 import org.challenge.input.PlayerInput;
+import org.challenge.model.Move;
+import org.challenge.model.Result;
 
 public class Game {
     private final PlayerInput playerInput;
@@ -28,6 +30,19 @@ public class Game {
         int computerWins = 0;
         int draws = 0;
 
+        for (int currentRound = 1; currentRound <= numOfRounds; currentRound++) {
+            System.out.printf("%n Round %d/%d %n", currentRound, numOfRounds);
+
+            Move playerMove = playerInput.getPlayerMove();
+            Move computerMove = computerMoveGenerator.generateComputerMove();
+            Result result = moveEvaluator.evaluateResult(playerMove, computerMove);
+
+            switch (result) {
+                case WIN -> playerWins++;
+                case LOSE -> computerWins++;
+                case DRAW -> draws++;
+            }
+        }
         System.out.printf("%n Final Results: WIN:%d LOSE:%d DRAW:%d %n", playerWins, computerWins, draws);
     }
 }
