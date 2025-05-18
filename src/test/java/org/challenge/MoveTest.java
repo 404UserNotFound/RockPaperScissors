809fb0a2d7abf19880adfc8e6bc80e057bdf8342
Mih <mihaelabrodetchi@gmail.com>
2025-05-18 16:40:53 +0100

    *unit test Move winConditions *test unhappy paths with invalid input/null input

---
+++ src/test/java/org/challenge/MoveTest.java
@@ -0,0 +1,40 @@
+package org.challenge;
+
+import org.challenge.model.Move;
+import org.challenge.model.Result;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoveTest {
+
+    @Test
+    public void rockBeatsScissors() {
+        assertTrue(Move.ROCK.winCondition(Move.SCISSORS));
+    }
+
+    @Test
+    public void paperBeatsRock() {
+        assertTrue(Move.PAPER.winCondition(Move.ROCK));
+    }
+
+    @Test
+    public void scissorsBeatsPaper() {
+        assertTrue(Move.SCISSORS.winCondition(Move.PAPER));
+    }
+
+    @Test
+    public void paperDrawsWithPaper() {
+        assertEquals(Result.DRAW, Move.PAPER.compareMoves(Move.PAPER));
+    }
+
+    @Test
+    void throwExceptionWhenInputInvalid() {
+        assertThrows(IllegalArgumentException.class, () -> Move.valueOf("INVALID_MOVE"));
+    }
+
+    @Test
+    void throwNullPointerExceptionWhenInputNull() {
+        assertThrows(NullPointerException.class, () -> Move.valueOf(null));
+    }
+}
