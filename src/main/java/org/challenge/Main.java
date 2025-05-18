52bb16b5e77c750c160bf0746fe5cff2a7810641
Mih <mihaelabrodetchi@gmail.com>
2025-05-18 15:50:37 +0100

    *Get input for number of rounds to main class *add user input exception handler method *instantiate methods to get user move input, generate computer move, and the moveEvaluator

--- src/main/java/org/challenge/Main.java
+++ src/main/java/org/challenge/Main.java
@@ -1,7 +1,39 @@
 package org.challenge;
 
+import org.challenge.computer.ComputerMoveGenerator;
+import org.challenge.computer.ComputerMoveGeneratorImpl;
+import org.challenge.evulator.MoveEvaluator;
+import org.challenge.evulator.MoveEvaluatorImpl;
+import org.challenge.input.PlayerInput;
+import org.challenge.input.PlayerInputImpl;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome, please input how many rounds you would like to play: ");
+        int numOfRounds = validateNumOfRounds(scanner);
+
+        PlayerInput playerInput = new PlayerInputImpl(scanner);
+        ComputerMoveGenerator computerMove = new ComputerMoveGeneratorImpl();
+        MoveEvaluator moveEvaluator = new MoveEvaluatorImpl();
+
+    }
+
+    private static int validateNumOfRounds(Scanner scanner) {
+        while (true) {
+            try {
+                String input = scanner.nextLine();
+                int rounds = Integer.parseInt(input);
+                if (rounds <= 0) {
+                    throw new NumberFormatException();
+                }
+                return rounds;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter a positive integer: ");
+            }
+
+        }
     }
 }
